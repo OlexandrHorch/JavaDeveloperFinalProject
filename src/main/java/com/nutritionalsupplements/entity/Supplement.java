a6d6e063bb443a9172ba0277ba6537812a7814bb
ctpahhuk22@gmail.com <gathering>
2019-03-29 03:00:40 +0200

    Entity Supplement with Lombok and with right names of Enum values (from the small latter)

--- src/main/java/com/nutritionalsupplements/entity/Supplement.java
+++ src/main/java/com/nutritionalsupplements/entity/Supplement.java
@@ -1,7 +1,10 @@
 package com.nutritionalsupplements.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name = "supplements")
 public class Supplement {
@@ -14,15 +17,15 @@ public class Supplement {
     private String name;
 
     @Enumerated(value = EnumType.STRING)
-    @Column(name = "Category")
+    @Column(name = "category")
     private SupplementEnumCategory Category = SupplementEnumCategory.ANTIOXIDANTS;
 
     @Enumerated(value = EnumType.STRING)
-    @Column(name = "Danger")
+    @Column(name = "danger")
     private SupplementEnumDanger Danger = SupplementEnumDanger.VERY_LOW;
 
     @Enumerated(value = EnumType.STRING)
-    @Column(name = "Origin")
+    @Column(name = "origin")
     private SupplementEnumOrigin Origin = SupplementEnumOrigin.ANIMAL;
 
     @Column(name = "using")
@@ -39,85 +42,4 @@ public class Supplement {
 
     @Column(name = "legislation")
     private String legislation;
-
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public SupplementEnumCategory getCategory() {
-        return Category;
-    }
-
-    public void setCategory(SupplementEnumCategory category) {
-        Category = category;
-    }
-
-    public SupplementEnumDanger getDanger() {
-        return Danger;
-    }
-
-    public void setDanger(SupplementEnumDanger danger) {
-        Danger = danger;
-    }
-
-    public SupplementEnumOrigin getOrigin() {
-        return Origin;
-    }
-
-    public void setOrigin(SupplementEnumOrigin origin) {
-        Origin = origin;
-    }
-
-    public String getUsing() {
-        return using;
-    }
-
-    public void setUsing(String using) {
-        this.using = using;
-    }
-
-    public String getHarm() {
-        return harm;
-    }
-
-    public void setHarm(String harm) {
-        this.harm = harm;
-    }
-
-    public String getBenefit() {
-        return benefit;
-    }
-
-    public void setBenefit(String benefit) {
-        this.benefit = benefit;
-    }
-
-    public String getGeneralInfo() {
-        return generalInfo;
-    }
-
-    public void setGeneralInfo(String generalInfo) {
-        this.generalInfo = generalInfo;
-    }
-
-    public String getLegislation() {
-        return legislation;
-    }
-
-    public void setLegislation(String legislation) {
-        this.legislation = legislation;
-    }
 }
