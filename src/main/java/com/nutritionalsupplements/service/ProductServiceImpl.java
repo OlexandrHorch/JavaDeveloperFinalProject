4a5641a219132cf6d3c6606e56bf19c5e5e111d9
ctpahhuk22@gmail.com <gathering>
2019-03-30 17:24:27 +0200

    Supplement service interfaces was added and their's implementation with implemented methods.

--- src/main/java/com/nutritionalsupplements/service/ProductServiceImpl.java
+++ src/main/java/com/nutritionalsupplements/service/ProductServiceImpl.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface ProductService {
+public class ProductServiceImpl {
+    @Autowired
+    private ProductRepository productRepository;
 
 
 
---
+++ src/main/java/com/nutritionalsupplements/service/SupplementServiceImpl.java
@@ -0,0 +1,44 @@
+package com.nutritionalsupplements.service;
+
+import com.nutritionalsupplements.entity.Supplement;
+import com.nutritionalsupplements.repository.SupplementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+public class SupplementServiceImpl implements SupplementService {
+
+    @Autowired
+    private SupplementRepository supplementRepository;
+
+    @Override
+    public Supplement getSupplement(Long id) {
+        return supplementRepository.getOne(id);
+    }
+
+    @Override
+    public void saveSupplement(Supplement supplement) {
+        supplementRepository.save(supplement);
+    }
+
+    @Override
+    public List<Supplement> getSupplements() {
+        return supplementRepository.findAll();
+    }
+
+    @Override
+    public void updateSupplement(Supplement supplement) {
+        final Supplement supplementForUpdate = this.supplementRepository.findById(supplement.getId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        supplementRepository.save(supplementForUpdate);
+    }
+
+    @Override
+    public void removeSupplement(Supplement supplement) {
+        supplementRepository.delete(supplement);
+    }
+}
