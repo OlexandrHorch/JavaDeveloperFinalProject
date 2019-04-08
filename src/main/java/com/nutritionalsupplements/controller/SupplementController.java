db1a56114667a6061f95a672a67efe889d8ce115
VeselovaAnna <Lavrav@ukr.net>
2019-04-08 20:40:34 +0300

    написала метод changeSupplement и подправила FindByName. Но сотавила два метода на всякий случай

--- src/main/java/com/nutritionalsupplements/controller/SupplementController.java
+++ src/main/java/com/nutritionalsupplements/controller/SupplementController.java
@@ -6,6 +6,9 @@ import com.nutritionalsupplements.service.SupplementService;
 import com.nutritionalsupplements.service.SupplementSpecifications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class SupplementController {
@@ -14,5 +17,21 @@ public class SupplementController {
 
     private Supplement findByCodeName (String codeName){
         return supplementService.query(SupplementSpecifications.withNameContaining(codeName)).get(0);
+    }
+
+
+    @RequestMapping(value = "/supplement", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Supplement> findByName (@RequestParam("name") String codeName){
+        return supplementService.query(SupplementSpecifications.withNameContaining(codeName));
+    }
+
+
+    @PostMapping
+    @ResponseBody
+    public Supplement changeSupplement(@RequestParam(name = "id") Long id, @RequestBody Supplement supplement) {
+        supplement.setId(id);
+        supplementService.updateSupplement(supplement);
+        return supplement;
     }
 }
