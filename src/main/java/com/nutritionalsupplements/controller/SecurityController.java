735672f0bd75c05014aa71c4a99dab42a8b7653a
ctpahhuk22@gmail.com <gathering>
2019-04-08 20:51:16 +0200

    finished "add supplement" in the AdminController with html form and getAllSupplements in SupplementController

--- src/main/java/com/nutritionalsupplements/controller/SecurityController.java
+++ src/main/java/com/nutritionalsupplements/controller/SecurityController.java
@@ -1,12 +1,19 @@
 package com.nutritionalsupplements.controller;
 
+import com.nutritionalsupplements.service.security.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SecurityController {
+
+    @Autowired
+    UserService userService;
+
     @GetMapping("/login")
     public String login() {
         return "security/login";
     }
+
 }
--- src/main/java/com/nutritionalsupplements/controller/SupplementController.java
+++ src/main/java/com/nutritionalsupplements/controller/SupplementController.java
@@ -6,13 +6,39 @@ import com.nutritionalsupplements.service.SupplementService;
 import com.nutritionalsupplements.service.SupplementSpecifications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class SupplementController {
+
     @Autowired
     private SupplementService supplementService;
+
+    @GetMapping("/get-supp/{id}")
+    public String getSupplementById(@PathVariable(name = "id") Long id, Model model) {
+
+        Supplement supplement = supplementService.getSupplement(id);
+        model.addAttribute("supplement", supplement);
+
+        return "supplement";
+    }
+
+    @GetMapping("/supplements")
+    public String getSuplements(Model model) {
+
+        List<Supplement> supplements = supplementService.getSupplements();
+
+        model.addAttribute("supplements", supplements);
+
+        return "supplements";
+    }
 
     private Supplement findByCodeName (String codeName){
         return supplementService.query(SupplementSpecifications.withNameContaining(codeName)).get(0);
     }
+
 }
