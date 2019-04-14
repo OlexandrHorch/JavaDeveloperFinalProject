a770bc17ed24b6bd23ae05db4d44eb6e686be963
Ivan Melnichuk <melnichuk.cadet@gmail.com>
2019-04-14 20:32:19 +0300

    Fixed additive columns

--- src/main/java/com/additives/entity/Additive.java
+++ src/main/java/com/additives/entity/Additive.java
@@ -20,6 +20,7 @@ public class Additive {
     @Column(name = "name")
     private String name;
 
+    @Lob
     @Column(name = "other_names", length = 2000)
     private String otherNames;
 
@@ -39,15 +40,19 @@ public class Additive {
     @Column(name = "using_info", length = 5000)
     private String usingInfo;
 
+    @Lob
     @Column(name = "harm", length = 5000)
     private String harm;
 
+    @Lob
     @Column(name = "benefit", length = 5000)
     private String benefit;
 
+    @Lob
     @Column(name = "general_info", length = 5000)
     private String generalInfo;
 
+    @Lob
     @Column(name = "legislation", length = 5000)
     private String legislation;
 }
